2289b89f5a3eb322b6e26c94909430d84b1e380a
Subash Bhattachan <bash_shoe@hotmail.com>
2024-12-08 21:38:16 -0500

    css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,100 +8,105 @@ import './App.css'
 function App() {
   const [learnerData, setLearnerData] = useState({
     learners: [
-        {
-          name: 'Cait Yomorta',
-          bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
-          scores: [
-            {
-              date: '2018-02-08',
-              score: 77
-            },
-            {
-              date: '2018-04-22',
-              score: 92
-            },
-            {
-              date: '2018-09-15',
-              score: 68
-            }
-          ]
-        },
-        {
-          name: 'Holly Baird',
-          bio: 'Eum molestiae explicabo deserunt, maiores quod eaque omnis tenetur vero ducimus, magnam autem! Quia facere quaerat eum repudiandae dolorum eligendi iure quae. Eos id possimus accusantium, earum animi modi hic.',
-          scores: [
-            {
-              date: '2018-12-14',
-              score: 88
-            },
-            {
-              date: '2019-01-09',
-              score: 79
-            },
-            {
-              date: '2019-02-23',
-              score: 91
-            },
-            {
-              date: '2019-03-01',
-              score: 95
-            }
-          ]
-        },
-        {
-          name: 'Wes Mungia',
-          bio: 'Repudiandae veritatis recusandae quidem tenetur impedit, numquam incidunt enim, adipisci id cupiditate asperiores nam perferendis. Facere odit laborum ipsum autem repellendus natus eius doloremque ullam perferendis. Enim repellendus ut veniam?',
-          scores: [
-            {
-              date: '2018-10-11',
-              score: 62
-            },
-            {
-              date: '2018-11-24',
-              score: 74
-            },
-            {
-              date: '2018-12-19',
-              score: 85
-            }
-          ]
-        }
-      ]
+      {
+        name: 'Cait Yomorta',
+        bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
+        scores: [
+          {
+            date: '2018-02-08',
+            score: 77
+          },
+          {
+            date: '2018-04-22',
+            score: 92
+          },
+          {
+            date: '2018-09-15',
+            score: 68
+          }
+        ]
+      },
+      {
+        name: 'Holly Baird',
+        bio: 'Eum molestiae explicabo deserunt, maiores quod eaque omnis tenetur vero ducimus, magnam autem! Quia facere quaerat eum repudiandae dolorum eligendi iure quae. Eos id possimus accusantium, earum animi modi hic.',
+        scores: [
+          {
+            date: '2018-12-14',
+            score: 88
+          },
+          {
+            date: '2019-01-09',
+            score: 79
+          },
+          {
+            date: '2019-02-23',
+            score: 91
+          },
+          {
+            date: '2019-03-01',
+            score: 95
+          }
+        ]
+      },
+      {
+        name: 'Wes Mungia',
+        bio: 'Repudiandae veritatis recusandae quidem tenetur impedit, numquam incidunt enim, adipisci id cupiditate asperiores nam perferendis. Facere odit laborum ipsum autem repellendus natus eius doloremque ullam perferendis. Enim repellendus ut veniam?',
+        scores: [
+          {
+            date: '2018-10-11',
+            score: 62
+          },
+          {
+            date: '2018-11-24',
+            score: 74
+          },
+          {
+            date: '2018-12-19',
+            score: 85
+          }
+        ]
+      }
+    ]
   })
 
 
   return (
     <>
-    <div className="LearnersApp">
-      <h1>Learners</h1>
-      {learnerData.learners.map((learner, index) => (
-        <div key= {index} className="learner">
-          <h2>Learner Data</h2>
-          <p>{learner.name}</p>
-          <p>{learner.bio}</p>
+      <div className="LearnersInfo">
+        <h1>Learners</h1>
 
-          <h3>Scores Data</h3>
-
-          {/* {learner.scores.map((score, scoreIndex) => {
-            <li key={scoreIndex}>
-            <p>Data: {score.date}</p>
-            <p>Score: {score.score}</p>
-            </li>
-
-          })} */}
+        {learnerData.learners.map((learner, index) => (
+          // rendering the learners info 
+          <div key={index} className="learner">
+            <h2>Learner Data</h2>
+            <p className="learner-name">{learner.name}</p>
+            <p>{learner.bio}</p>
 
 
+            <h2>Scores Data</h2>
+            <div className="ScoresInfo">
+              <ul>
+              {learner.scores.map((score, scoreIndex) => (
+                // rendering the scores
+                <li key={scoreIndex}>
+                  <p>Date: {score.date}</p>
+                  <p>Score: {score.score}</p>
+                </li>
+
+              ))}
+              </ul>
+            </div>
 
           </div>
 
 
 
-      ))}
+        ))}
 
 
 
 
-    </div>
+      </div>
 
     </>
   )
